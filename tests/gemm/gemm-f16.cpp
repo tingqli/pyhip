ecf574ca665da3ee4e5a0a84e7b194a8d99b111f
Li, Tingqian <tingqli@amd.com>
2025-10-28 22:32:03 +0800

    [gemm] refactor

--- tests/gemm/gemm-f16.cpp
+++ tests/gemm/gemm-f16.cpp
@@ -34,6 +34,17 @@ using float32x16 = __attribute__((__vector_size__(16 * sizeof(float)))) float;
 using int32x4_t = __attribute__((__vector_size__(4 * sizeof(int)))) int;
 using int32x16_t = __attribute__((__vector_size__(16 * sizeof(int)))) int;
 
+__device__ __inline__
+static uint buffer_load_dword(int32x4_t srsrc,
+                        int32_t voffset,
+                        int32_t soffset,
+                        int32_t aux) __asm("llvm.amdgcn.raw.buffer.load.i32");
+__device__ __inline__
+static int32x4_t buffer_load_dwordx4(int32x4_t srsrc,
+                        int32_t voffset,
+                        int32_t soffset,
+                        int32_t aux) __asm("llvm.amdgcn.raw.buffer.load.v4i32");
+
 union BufferResource {
     __device__ __inline__ constexpr BufferResource()
         : config(0x00020000U) {}
@@ -49,18 +60,11 @@ union BufferResource {
         uint32_t range;     // Byte range for the buffer resource
         uint32_t config;    // Constant, DFMT=32b
     };
+    __device__ __inline__ int32x4_t load_dwordx4(int32_t voffset, int32_t soffset) {
+        return buffer_load_dwordx4(descriptor, voffset, soffset, 0);
+    }
 };
 
-__device__ __inline__
-static uint buffer_load_dword(int32x4_t srsrc,
-                        int32_t voffset,
-                        int32_t soffset,
-                        int32_t aux) __asm("llvm.amdgcn.raw.buffer.load.i32");
-__device__ __inline__
-static int32x4_t buffer_load_dwordx4(int32x4_t srsrc,
-                        int32_t voffset,
-                        int32_t soffset,
-                        int32_t aux) __asm("llvm.amdgcn.raw.buffer.load.v4i32");
 /*
     thread block level tile copy
 
@@ -75,97 +79,148 @@ static int32x4_t buffer_load_dwordx4(int32x4_t srsrc,
         lane0 lane1 lane2 lane3
         lane4 lane5 lane6 lane7 
         ... ...
+
+how to load 2D-tile from HBM ?
+use load_dwordx4, lane-offset from tile-shape
+
+load from LDS into MFMA layout
+store to LDS normal layout : col-major
+
 */
+
+// SFINAE
+// https://www.cppstories.com/2016/02/notes-on-c-sfinae/
+// https://stackoverflow.com/questions/48045559/how-do-i-declare-sfinae-class
 
 template<int shift, int mask>
 __device__ __inline__ int swizzle_col(int logical_row, int logical_col) {
     return (logical_col^(logical_row >> shift))&(mask);
 }
 
-struct block_copy_256x32 {
-    int32x4_t d4x4[4];
-    __device__ void prefetch(BufferResource& buff, int soffset, int nstride) {
-        auto lane_offset = ((threadIdx.x & 3)*8 + (threadIdx.x >> 2) * nstride) * sizeof(__fp16);
-        //auto lane_offset = ((threadIdx.x & 7)*8 + (threadIdx.x >> 3) * nstride) * sizeof(__fp16);
-        //auto lane_offset = (threadIdx.x * 8) * sizeof(__fp16);
-        d4x4[0] = buffer_load_dwordx4(buff.descriptor, lane_offset, (soffset + 0)*sizeof(__fp16), 0);
-        d4x4[1] = buffer_load_dwordx4(buff.descriptor, lane_offset, (soffset + 64*1*nstride)*sizeof(__fp16), 0);
-        d4x4[2] = buffer_load_dwordx4(buff.descriptor, lane_offset, (soffset + 64*2*nstride)*sizeof(__fp16), 0);
-        d4x4[3] = buffer_load_dwordx4(buff.descriptor, lane_offset, (soffset + 64*3*nstride)*sizeof(__fp16), 0);
-    }
-    __device__ void store(__fp16* pdst, int nstride) {
-        auto lane_offset = ((threadIdx.x & 3)*8 + (threadIdx.x >> 2) * nstride);
-        *(int32x4_t*)(pdst + lane_offset + 0) = d4x4[0];
-        *(int32x4_t*)(pdst + lane_offset + 64*1*nstride) = d4x4[1];
-        *(int32x4_t*)(pdst + lane_offset + 64*2*nstride) = d4x4[2];
-        *(int32x4_t*)(pdst + lane_offset + 64*3*nstride) = d4x4[3];
-    }
-    template<int nstride>
-    __device__ void store(__fp16* pdst) {
-        auto row = (threadIdx.x >> 2);
-        auto col = swizzle_col<1, 3>(row, threadIdx.x & 3);
-        auto lane_offset = (col*8 + row * nstride);
-        *(int32x4_t*)(pdst + lane_offset + 0) = d4x4[0];
-        *(int32x4_t*)(pdst + lane_offset + 64*1*nstride) = d4x4[1];
-        *(int32x4_t*)(pdst + lane_offset + 64*2*nstride) = d4x4[2];
-        *(int32x4_t*)(pdst + lane_offset + 64*3*nstride) = d4x4[3];
+template<int shift, int mask>
+struct SwizzleCol {
+    __device__ __inline__ static int swizzle(int logical_row, int logical_col) {
+        return (logical_col^(logical_row >> shift))&(mask);
     }
 };
+
+
+constexpr bool is_powerof2(int v) {
+    return v && ((v & (v - 1)) == 0);
+}
+
+constexpr int clog2(int val) {
+    if (val == 1) return 0;
+    return 1 + clog2(val >> 1);
+}
+
+/*
+    2D tile at Work-group-level
+
+    threads: number of threads in work-group
+*/
+using DWORD4 = int32x4_t;
+template<typename T, int rows, int cols, int nthreads>
+struct WGTile {
+    static_assert((sizeof(T) * cols) % sizeof(DWORD4) == 0, "cannot load a row using DWORD4");
+
+    static constexpr int warp_Size = 64;
+
+    static constexpr int lane_cols = sizeof(T) * cols / sizeof(DWORD4);
+
+    static_assert(is_powerof2(lane_cols));
+    static_assert(warp_Size % lane_cols == 0, "cannot load all rows using all lanes");
+
+    static constexpr int wg_rows = nthreads / lane_cols;
+    static constexpr int lane_rows = warp_Size / lane_cols;
+    static constexpr int lane_col_shift = clog2(lane_cols);
+
+    static_assert(rows % wg_rows == 0, "cannot load all rows using all threads");
+    static constexpr int num_dwordx4 = rows / wg_rows;
+
+    __device__ static int voff(int nstride) {
+        return (threadIdx.x & (lane_cols-1))*sizeof(DWORD4) + (threadIdx.x >> lane_col_shift) * nstride * sizeof(T);
+    }
+
+    // register temp
+    int32x4_t data[num_dwordx4];
+
+    __device__ void prefetch(BufferResource& buffer, int soffset, int nstride) {
+        auto lane_offset = voff(nstride);
+        #pragma unroll
+        for(int r = 0; r < num_dwordx4; r++) {
+            // one such load at WG level produces lane_rows
+            data[r] = buffer.load_dwordx4(lane_offset, (soffset + r * wg_rows * nstride)*sizeof(T));    
+        }
+    }
+
+    template<int nstride, typename F>
+    __device__ void store(T* pdst) {
+        auto row = (threadIdx.x >> lane_col_shift);
+        auto col = F::swizzle(row, threadIdx.x & (lane_cols-1));
+        auto lane_offset = (col*sizeof(DWORD4)/sizeof(T) + row * nstride);
+        #pragma unroll
+        for(int r = 0; r < num_dwordx4; r++) {
+            *(int32x4_t*)(pdst + lane_offset + r * wg_rows * nstride) = data[r];
+        }
+    }
+};
+
+
+/*
+Warp level MFMA A/B register
+*/
+template<int BM, int BK, int IM=32, int IK=8>
+struct Warp_MFMA_AB {
+    static_assert((IM == 32 && IK == 8));
+    static_assert((BM % IM) == 0);
+    static_assert((BK % IK) == 0);
+
+    static constexpr int M_shift = clog2(IM);
+    static constexpr int nM = BM / IM;
+    static constexpr int nK = BK / IK;
+
+    union ABType {
+        // (M=32,K=8x4) halfs, each lane has 16 halfs
+        float16x4 h[nK]; // 32x8[x4]xhalfs
+        int32x4_t d[nK/2]; // 2 * 32x16xhalfs
+    } regs[nM];
+
+    template<int nstride>
+    __device__ void load(__fp16 * buff) {
+        int lane = threadIdx.x & 63;
+        #pragma unroll
+        for(int ik = 0; ik < nK/2; ik ++) {
+            auto row = (lane & (IM-1));
+            auto col = swizzle_col<1,3>(row, (lane >> M_shift) + ik*2);
+            auto lane_off = row * nstride + col * 8;
+            #pragma unroll
+            for(int m = 0; m < nM; m++) {
+                regs[m].d[ik] = *(int32x4_t*)(buff + lane_off + m*32*nstride);
+            }
+        }
+    }
+};
+
+using ABTile = WGTile<__fp16, 256, 32, 256>;
+using ABRegs = Warp_MFMA_AB<128, 32>;
+
 
 #define SGB_VMEM_read_0x0020 0x0020
 #define SGB_MFMA_0x0008      0x0008
 #define SGB_DS_read_0x0100   0x0100
 #define SGB_DS_write_0x0200  0x0200
-/*
-__global__ void __launch_bounds__(256, 1) xxxtile(int* A, int N, int * B) {
-    union bigtype {
-        int32x4_t dwx4[2];
-        float16x4 dhx4[4];
-    } buff;
 
-    BufferResource bufferA(A, N * 128 * sizeof(__fp16));
-
-    auto lane_offset = ((threadIdx.x & 3)*8 + (threadIdx.x >> 2) * 256) * sizeof(__fp16);
-    buff.dwx4[0] = buffer_load_dwordx4(bufferA.descriptor, lane_offset, (64*0)*sizeof(int), 0);
-    buff.dwx4[1] = buffer_load_dwordx4(bufferA.descriptor, lane_offset, (64*1)*sizeof(int), 0);
-
-    float32x16 c = {};
-    for(int i=0; i < N; i++) {
-
-        c = __builtin_amdgcn_mfma_f32_32x32x8f16(buff.dhx4[0], buff.dhx4[1], c, 0, 0, 0);
-        c = __builtin_amdgcn_mfma_f32_32x32x8f16(buff.dhx4[2], buff.dhx4[3], c, 0, 0, 0);
-
-        buff.dwx4[0] = buffer_load_dwordx4(bufferA.descriptor, lane_offset, (64*i)*sizeof(int), 0);
-        buff.dwx4[1] = buffer_load_dwordx4(bufferA.descriptor, lane_offset, (64*i+32)*sizeof(int), 0);
-
-        __builtin_amdgcn_sched_group_barrier(SGB_MFMA_0x0008, 1, 0);
-        __builtin_amdgcn_sched_group_barrier(SGB_VMEM_read_0x0020, 1, 0);
-        __builtin_amdgcn_sched_group_barrier(SGB_MFMA_0x0008, 1, 0);
-        __builtin_amdgcn_sched_group_barrier(SGB_VMEM_read_0x0020, 1, 0);
-    }
-    *(float32x16*)B = c;
-}
-
-*/
 __global__ void __launch_bounds__(256, 1) gemm_tile_256x256x32(
         __fp16* A, __fp16* B, int nstrideAB, float* C, int nstrideC,
-        int nblkK, int nblkM, int nblkN) {
+        int nblkK, int* blk_maps) {
     int warp_id = __builtin_amdgcn_readfirstlane(threadIdx.x >> 6);
     int lane = threadIdx.x & 63;
 
     auto blk_index = blockIdx.x;
-#if 0
-    // naive block <-> CU mapping
-    auto blkY = blk_index / nblkN;
-    auto blkX = blk_index % nblkN;
-#else
-    // mapps blocks within same XCC/XCD closer
-    // here we assume CU's of 4 XCC/XCD distributed into a 2x2 Tile
-    auto xcc_id = blk_index & 3;
-    blk_index = blk_index >> 2;
-    auto blkY = (blk_index / (nblkN/2)) + (xcc_id >> 1)*(nblkM/2);
-    auto blkX = (blk_index % (nblkN/2)) + (xcc_id & 1)*(nblkN/2);
-#endif
+    auto blkY = blk_maps[blk_index*2 + 0];
+    auto blkX = blk_maps[blk_index*2 + 1];
+
     A += blkY * 256 * nstrideAB;
     B += blkX * 256 * nstrideAB;
     C += blkX * 256 + blkY * 256 * nstrideC;
@@ -176,32 +231,8 @@ __global__ void __launch_bounds__(256, 1) gemm_tile_256x256x32(
     __shared__ __fp16 Abuff[256*32*2];
     __shared__ __fp16 Bbuff[256*32*2];
 
-    block_copy_256x32 copyA;
-    block_copy_256x32 copyB;
-
     constexpr int lds_nstride = 32;
 
-    struct ABRegs {
-        union ABType {
-            // (M=32,K=8x4) halfs, each lane has 16 halfs
-            float16x4 h[4]; // 32x8[x4]xhalfs
-            int32x4_t d[2]; // 2 * 32x16xhalfs
-        } regs[4];
-
-        __device__ void load(__fp16 * buff) {
-            int warp_id = __builtin_amdgcn_readfirstlane(threadIdx.x >> 6);
-            int lane = threadIdx.x & 63;
-            auto LDS0 = ((0 + 0)&1)*256*32;
-            for(int ik = 0; ik < 2; ik ++) {
-                auto row = (lane & 31);
-                auto col = swizzle_col<1,3>(row, (lane >> 5) + ik*2);
-                auto lane_off = row * lds_nstride + col * 8;
-                for(int m = 0; m < 4; m++) {
-                    regs[m].d[ik] = *(int32x4_t*)(buff + lane_off + m*32*lds_nstride);
-                }
-            }
-        }
-    };
     ABRegs a[2];
     ABRegs b[2];
     auto* Abuff_warp = Abuff + (warp_id >> 1)*32*4*lds_nstride;
@@ -215,89 +246,64 @@ __global__ void __launch_bounds__(256, 1) gemm_tile_256x256x32(
                     auto i = m*4 + n;
                     c[i] = __builtin_amdgcn_mfma_f32_32x32x8f16(
                                 a.regs[m].h[ik], b.regs[n].h[ik], c[i], 0, 0, 0);
+                    //asm("v_mfma_f32_32x32x8_f16 %0, %1, %2, %3\n"
+                    //            : "+a"(c[i])
+                    //            : "v"(a.regs[m].h[ik]), "v"(b.regs[n].h[ik]), "a"(c[i])
+                    //            :);
                 }
             }
         }
     };
 
-    // prefetch(data0) to regs
-    // store regs(data0) to LDS1
-    // prefetch(data1) to regs
-    //
-    // for:
-    //  __syncthreads();
-    //     switch LDS0 & 1
-    //     MFMA(data0) on LDS0
-    //     store regs(data1) to LDS1
-    //     prefetch(data2) to regs
-    //
-  
-    /*
-              copy.prefetch(data0)
-              copy.store(data0, LDS0)
+    ABTile tileA;
+    ABTile tileB;
 
-              copy.prefetch(data1)
+    // prelog
+    using swizzle = SwizzleCol<1, 3>;
+    tileA.prefetch(bufferA, 0*32, nstrideAB);
+    tileB.prefetch(bufferB, 0*32, nstrideAB);
+    tileA.store<32,swizzle>(Abuff);
+    tileB.store<32,swizzle>(Bbuff);
 
-              __syncthreads();
-              copy.store(data1, LDS1)
-              copy.prefetch(data2)
-              REG1.lds_load(data0, LDS0)
-        for:
-              __syncthreads();
-
-              copy.store(data2, LDS0)
-              copy.prefetch(data3)
-
-              REG1.MFMA(data0)
-              REG1.lds_load(data1, LDS1)
-
-              swap LDS0 with LDS1
-    */
-
-    copyA.prefetch(bufferA, 0*32, nstrideAB);
-    copyB.prefetch(bufferB, 0*32, nstrideAB);
-    copyA.store<32>(Abuff);
-    copyB.store<32>(Bbuff);
-    copyA.prefetch(bufferA, 1*32, nstrideAB);
-    copyB.prefetch(bufferB, 1*32, nstrideAB);
+    tileA.prefetch(bufferA, 1*32, nstrideAB);
+    tileB.prefetch(bufferB, 1*32, nstrideAB);
 
     __syncthreads();
-    a[0].load(Abuff_warp + 0);
-    b[0].load(Bbuff_warp + 0);
-    copyA.store<32>(Abuff + 256*32);
-    copyB.store<32>(Bbuff + 256*32);
-    copyA.prefetch(bufferA, 2*32, nstrideAB);
-    copyB.prefetch(bufferB, 2*32, nstrideAB);
-    
+    a[0].load<lds_nstride>(Abuff_warp + 0);
+    b[0].load<lds_nstride>(Bbuff_warp + 0);
+    tileA.store<32,swizzle>(Abuff + 256*32);
+    tileB.store<32,swizzle>(Bbuff + 256*32);
+    tileA.prefetch(bufferA, 2*32, nstrideAB);
+    tileB.prefetch(bufferB, 2*32, nstrideAB);
+
+    // body
     for(int ok = 0; ok < nblkK; ok += 2) {
         __syncthreads();
 
         constexpr auto LDS0 = 0;
         constexpr auto LDS1 = 256*32;
 
-        copyA.store<32>(Abuff + LDS0);
-        copyB.store<32>(Bbuff + LDS0);
-        copyA.prefetch(bufferA, (ok+3)*32, nstrideAB);
-        copyB.prefetch(bufferB, (ok+3)*32, nstrideAB);
+        tileA.store<32,swizzle>(Abuff + LDS0);
+        tileB.store<32,swizzle>(Bbuff + LDS0);
+        tileA.prefetch(bufferA, (ok+3)*32, nstrideAB);
+        tileB.prefetch(bufferB, (ok+3)*32, nstrideAB);
 
         mfma(a[0], b[0], c);
         // as long as MFMA instruction issued, we can start READ a&b register for next iteration
         // and no need to worry about RAW ?
-        a[1].load(Abuff_warp + LDS1);
-        b[1].load(Bbuff_warp + LDS1);
+        a[1].load<lds_nstride>(Abuff_warp + LDS1);
+        b[1].load<lds_nstride>(Bbuff_warp + LDS1);
 
-        //__builtin_amdgcn_sched_barrier(0);
         __syncthreads();
-        //asm volatile("s_waitcnt lgkmcnt(0)\n s_barrier\n");
 
-        copyA.store<32>(Abuff + LDS1);
-        copyB.store<32>(Bbuff + LDS1);
-        copyA.prefetch(bufferA, (ok+4)*32, nstrideAB);
-        copyB.prefetch(bufferB, (ok+4)*32, nstrideAB);
+        tileA.store<32,swizzle>(Abuff + LDS1);
+        tileB.store<32,swizzle>(Bbuff + LDS1);
+        tileA.prefetch(bufferA, (ok+4)*32, nstrideAB);
+        tileB.prefetch(bufferB, (ok+4)*32, nstrideAB);
 
         mfma(a[1], b[1], c);
-        a[0].load(Abuff_warp + LDS0);
-        b[0].load(Bbuff_warp + LDS0);
+        a[0].load<lds_nstride>(Abuff_warp + LDS0);
+        b[0].load<lds_nstride>(Bbuff_warp + LDS0);
 
         /*
         0x0200 DS write     ds_write_b128  x 8
--- tests/gemm/gemm-f16.py
+++ tests/gemm/gemm-f16.py
@@ -1,23 +1,38 @@
+import torch
 import pyhip
 import sys
 
-hip = pyhip.module("gemm-f16.cpp")
-
 import torch
-torch.cuda.set_device(3)
+torch.cuda.set_device(7)
 torch.set_default_device('cuda')
 torch.manual_seed(0)
 cur_gpu_device =torch.cuda.get_device_name()
-print(f"{torch.get_default_device()=}")
+num_CU = torch.cuda.get_device_properties().multi_processor_count
+NUM_XCD = 8 if num_CU > 80 else 4
+print(f"{torch.get_default_device()=} with {num_CU=} {NUM_XCD=}")
+    
+hip = pyhip.module("gemm-f16.cpp", f"-D {NUM_XCD=}")
 
+# find the square-like gemm shape that use as much CUs as possible
+min_gap = num_CU
+best_rows = -1
+for CU_rows in range(int(num_CU**0.5), 2, -1):
+    CU_cols = num_CU//CU_rows
+    gap = num_CU - CU_rows * CU_cols
+    if gap < min_gap:
+        min_gap = gap
+        best_rows = CU_rows
+        if gap == 0:
+            break
 
-num_CU = 80
-
-CU_rows = int(num_CU**0.5)
-CU_cols = num_CU//CU_rows
-M = 256 * CU_cols*1
-N = 256 * CU_rows*1
+CU_rows = 8 #best_rows
+CU_cols = 10 #num_CU//CU_rows
+M = 256 * CU_rows*1
+N = 256 * CU_cols*1
 K = 8192
+
+print(f" {M}x{N}x{K}  CUs: {CU_rows} x {CU_cols} = {CU_rows*CU_cols} ")
+
 StrideK = 128*1024
 # buffer_load_dwordx4's performance suddenly drops (by-half) when stride > 256KB
 # and hipblas's performance is better when StrideK=K*8 / StrideK=K*4
@@ -26,7 +41,28 @@ StrideK = K*1
 A = torch.randn(M, StrideK, dtype=torch.float16)
 B = torch.randn(N, StrideK, dtype=torch.float16)
 out = torch.randn(M, N, dtype=torch.float)
-
+blk_maps = torch.tensor([
+0, 0, 0, 5, 4, 0, 4, 5, 
+1, 0, 1, 5, 5, 0, 5, 5, 
+2, 0, 2, 5, 6, 0, 6, 5, 
+3, 0, 3, 5, 7, 0, 7, 5, 
+0, 1, 0, 6, 4, 1, 4, 6, 
+1, 1, 1, 6, 5, 1, 5, 6, 
+2, 1, 2, 6, 6, 1, 6, 6, 
+3, 1, 3, 6, 7, 1, 7, 6, 
+0, 2, 0, 7, 4, 2, 4, 7, 
+1, 2, 1, 7, 5, 2, 5, 7, 
+2, 2, 2, 7, 6, 2, 6, 7, 
+3, 2, 3, 7, 7, 2, 7, 7, 
+0, 3, 0, 8, 4, 3, 4, 8, 
+1, 3, 1, 8, 5, 3, 5, 8, 
+2, 3, 2, 8, 6, 3, 6, 8, 
+3, 3, 3, 8, 7, 3, 7, 8, 
+0, 4, 0, 9, 4, 4, 4, 9, 
+1, 4, 1, 9, 5, 4, 5, 9, 
+2, 4, 2, 9, 6, 4, 6, 9, 
+3, 4, 3, 9, 7, 4, 7, 9, 
+], dtype=torch.int32)
 gemm_tile_256x256x32 = hip.gemm_tile_256x256x32
 
 if len(sys.argv) == 1:
@@ -34,21 +70,21 @@ if len(sys.argv) == 1:
     # torch.nn.functional.linear(input, weight, bias=None)
     #   input:  (*, input_featires)
     #   Weight: (output_features, input_featires)
-    for i in range(2):
+    for i in range(8):
         with pyhip.cudaPerf(M*N*K*2, name="torch-linear"):
             ref = torch.nn.functional.linear(A[:,:K], B[:, :K])
     ref = ref.to(dtype=torch.float)
 
-    gemm_tile_256x256x32([(N//256)*(M//256)],[256], A.data_ptr(), B.data_ptr(), StrideK, out.data_ptr(), N, K//32, M//256, N//256)
+    gemm_tile_256x256x32([(N//256)*(M//256)],[256], A.data_ptr(), B.data_ptr(), StrideK, out.data_ptr(), N, K//32, blk_maps.data_ptr())
 
-    pass_flag = torch.allclose(ref, out, atol=0.01, rtol=0.01)
-    if not pass_flag:
-        print(ref)
-        print(out)
+    pass_flag = torch.allclose(ref, out, atol=0.1, rtol=0.1)
+    #if not pass_flag:
+    #    print(ref)
+    #    print(out)
 
-for i in range(2):
+for i in range(8):
     with pyhip.cudaPerf(M*N*K*2, (M*K*2+K*N*2), name="gemm_tile"):
-        gemm_tile_256x256x32([(N//256)*(M//256)],[256], A.data_ptr(), B.data_ptr(), StrideK, out.data_ptr(), N, K//32, M//256, N//256)
+        gemm_tile_256x256x32([(N//256)*(M//256)],[256], A.data_ptr(), B.data_ptr(), StrideK, out.data_ptr(), N, K//32, blk_maps.data_ptr())
 
 if len(sys.argv) == 1:
     print("PASS" if pass_flag else "FAILED")
