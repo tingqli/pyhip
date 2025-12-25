6e998b6f205366035f10bc41789ac95894aaf367
LuweiZhou <luwei.zhou@amd.com>
2025-12-25 14:26:50 +0800

    update with 32x32x8.

--- tests/gemm/gemm.cpp
+++ tests/gemm/gemm.cpp
@@ -14,7 +14,7 @@ using float32x4 = float  __attribute__ ((ext_vector_type(4)));
 // using float16x4 = __fp16 __attribute__ ((ext_vector_type(4)));
 // using float16x8 = __fp16 __attribute__ ((ext_vector_type(8)));
 // using float16x32 = __fp16 __attribute__ ((ext_vector_type(32)));
-// using float32x16 = float  __attribute__ ((ext_vector_type(16)));
+using float32x16 = float  __attribute__ ((ext_vector_type(16)));
 // using float32x4 = float  __attribute__ ((ext_vector_type(4)));
 // using float32x2 = float  __attribute__ ((ext_vector_type(2)));
 // using int32x4_t = int  __attribute__ ((ext_vector_type(4)));
@@ -29,33 +29,46 @@ __global__ void __launch_bounds__(256, 1) run_mfma(__fp16* A_ptr, __fp16* B_ptr,
 
     // printf("threadid:%d\n", threadIdx.x );
     int lane = threadIdx.x % 64;
-    int rowid = lane % 16;
-    int colid = lane / 16;
+    int rowid = lane % 32;
+    int colid = lane / 32;
     int wgid_m = blockIdx.x;
     int wgid_n = blockIdx.y;
     __fp16* A_warp = A_ptr + (wgid_m*BM + warp_id_m*WAVE_M)*K;
     __fp16* B_warp = B_ptr + (wgid_n*BN + warp_id_n*WAVE_N)*K;
     float* C_warp = C_ptr + (wgid_m*BM + warp_id_m*WAVE_M)*N + (wgid_n*BN + warp_id_n*WAVE_N);
 
-    float16x8 a[REG_M];
-    float16x8 b[REG_N];
-    float32x4 c[REG_M*REG_N] = {0};
+    //REGK is register factor based on MFMA ISA. Here 2 REGK are combined to meet minim 128 bit HBM read for coalescing tranction.
+    float16x8 a[REG_M*REG_K/2];
+    float16x8 b[REG_N*REG_K/2];
+    float32x16 c[REG_M*REG_N] = {0};
+    auto tmp_A = A_warp + rowid*K + colid*(REG_K*MFMA_KL);
     #pragma unroll
     for(int m = 0; m < REG_M; m++) {
-#if INTERLEAVE_ACCESS
-        a[m] = *(float16x8*)(A_warp + (rowid*REG_M + m)*K + colid*8);
-#else
-        a[m] = *(float16x8*)(A_warp + (rowid + m*16)*K + colid*8);
-#endif
-
+        #pragma unroll
+        //Contineous reading the REG_K*MFMA_KL
+        for (int i = 0; i < REG_K/2; i++) {
+            a[m*REG_K/2 + i] = *(float16x8*)(tmp_A+i*8);
+        }
+        tmp_A += 32*K;
     }
+    // {
+    //     auto& aa=a[0];
+    //     printf("[%d, colid:%d]: %f, %f, %f, %f, %f, %f, %f,%f\n", rowid, colid,aa[0],aa[1],aa[2],aa[3],aa[4],aa[5],aa[6],aa[7]);
+    // }
+    // {
+    //     auto& aa=a[1];
+    //     printf("[%d, colid:%d]: %f, %f, %f, %f, %f, %f, %f,%f\n", rowid, colid,aa[0],aa[1],aa[2],aa[3],aa[4],aa[5],aa[6],aa[7]);
+    // }
+
+
+    auto tmp_B = B_warp + rowid*K + colid*(REG_K*MFMA_KL);
     #pragma unroll
     for(int n = 0; n < REG_N; n++) {
-#if INTERLEAVE_ACCESS
-     b[n] = *(float16x8*)(B_warp + (rowid*REG_N + n)*K + colid*8);
-#else
-    b[n] = *(float16x8*)(B_warp + (rowid + n*16)*K + colid*8);
-#endif
+        #pragma unroll
+        for (int i = 0; i < REG_K/2; i++) {
+            b[n*REG_K/2 + i] = *(float16x8*)(tmp_B+i*8);
+        }
+        tmp_B += 32*K;
     }
 
     // for(int k = 0; k < K; k += BK) {
@@ -63,11 +76,12 @@ __global__ void __launch_bounds__(256, 1) run_mfma(__fp16* A_ptr, __fp16* B_ptr,
         for(int m = 0; m < REG_M; m++) {
             #pragma unroll
             for(int n = 0; n < REG_N; n++) {
-                auto i = m*REG_N + n;
-                // amdgcn_mfma_f32_16x16x16bf16(a[m].lo, b[n].lo, c[i]);
-                // amdgcn_mfma_f32_16x16x16bf16(a[m].hi, b[n].hi, c[i]);
-                c[i] = __builtin_amdgcn_mfma_f32_16x16x16f16(a[m].lo, b[n].lo, c[i], 0, 0, 0);
-                c[i] = __builtin_amdgcn_mfma_f32_16x16x16f16(a[m].hi, b[n].hi, c[i], 0, 0, 0);
+                auto idx = m*REG_N + n;
+                #pragma unroll
+                for (int i = 0; i < REG_K/2; i++) {
+                    c[idx] = __builtin_amdgcn_mfma_f32_32x32x8f16(a[m*REG_K/2 + i].lo, b[n*REG_K/2 + i].lo, c[idx], 0, 0, 0);
+                    c[idx] = __builtin_amdgcn_mfma_f32_32x32x8f16(a[m*REG_K/2 + i].hi, b[n*REG_K/2 + i].hi, c[idx], 0, 0, 0);
+                }
 
             }
         }
@@ -78,25 +92,20 @@ __global__ void __launch_bounds__(256, 1) run_mfma(__fp16* A_ptr, __fp16* B_ptr,
         for(int n = 0; n < REG_N; n++) {
             auto idx = m*REG_N + n;
             auto& v = c[idx];
-#if INTERLEAVE_ACCESS
-            float* p0 = C_warp + (m+ colid*4*REG_M)*N + n + rowid*REG_N;
-            *p0= v[0];
-            p0 += REG_M*N;
-            *p0 = v[1];
-            p0 += REG_M*N;
-            *p0 = v[2];
-            p0 += REG_M*N;
-            *p0 = v[3];
-#else
-            float* p0 = C_warp + (m*16+ colid*4)*N + n*16+rowid;
-            *p0= v[0];
-            p0 += N;
-            *p0 = v[1];
-            p0 += N;
-            *p0 = v[2];
-            p0 += N;
-            *p0 = v[3];
-#endif
+
+            float* p0 = C_warp + (m*32+ colid*4)*N + n*32+rowid;
+            #pragma unroll
+            for (int i = 0; i < 4; i++) {
+                *p0= v[i*4];
+                p0 += N;
+                *p0 = v[i*4+1];
+                p0 += N;
+                *p0 = v[i*4+2];
+                p0 += N;
+                *p0 = v[i*4+3];
+                //Move to next 8M.
+                p0 +=5*N;
+            }
         }
     }
 }
--- tests/gemm/test_gemm.py
+++ tests/gemm/test_gemm.py
@@ -5,30 +5,39 @@ import torch
 torch.cuda.set_device(2)
 torch.set_default_device('cuda')
 torch.manual_seed(0)
-torch.set_printoptions(linewidth=300)
+torch.set_printoptions(linewidth=500)
 
 cur_gpu_device =torch.cuda.get_device_name()
 print(f"{torch.get_default_device()=} {torch.cuda.device_count()}")
 
-#MFMA_F32_16x16x16_F16
+#MFMA_F32_32x32x8_F16
+MFMA_K=8
 MFMA_KL=4
+REG_KL=MFMA_KL//2
+#REG_M, REG_N means how many M, N per lane for A, B.
+#REG_KL and REG_K is a little tricky. 
+#       REG_KL means one MFMA need how many registers for A, B per lane. For 32x32x8, KL=4. So for FP16 datatype, REG_KL=2
+#       REG_K means the number of BK/MFMA_K
+#       Each lane in A needs (REG_M * REG_K * REG_KL) VGPRs. Each lane in B needs (REG_N * REG_K * REG_K)
+REG_M=4
+REG_N=4
+REG_KL=MFMA_KL//2
+REG_K=4
+assert REG_K % 2 == 0, f'REG_K % 2 == 0 to ensure 128 bit reading'
+
+
+WAVE_M = REG_M*32
+WAVE_N = REG_N*32
+BK = REG_K*MFMA_K
 #
-REG_M=48
-REG_N=48
-#
-WAVE_M = REG_M * 16
-WAVE_N = REG_N * 16
-REG_K=2
-BK = REG_K * 16
-#
-M_WAVES=2
-N_WAVES=2
+M_WAVES=1
+N_WAVES=1
 #
 BM=M_WAVES*WAVE_M
 BN=N_WAVES*WAVE_N
 
-M_WGS=10
-N_WGS=8
+M_WGS=1
+N_WGS=1
 K_BLKS=1
 
 M=BM*M_WGS
@@ -49,13 +58,16 @@ print(f'////////////////////////////////////////////////////////////////////////
 print(f'{M=}, {N=}, {K=}, {BM=},{BN=},{REG_M=}, {REG_N=}, GRID_DIM:[{M_WGS},{N_WGS}], BLOCK_DIM[{64*M_WAVES*N_WAVES}]')
 print(f'////////////////////////////////////////////////////////////////////////////////////////')
 
-hip = pyhip.module("gemm.cpp", f"-D{BM=} -D{BN=} -D{BK=} -D{WAVE_M=} -D{WAVE_N=}  -D{REG_M=} -D{REG_N=} -D{REG_K=} -D{N_WAVES=}")
+hip = pyhip.module("gemm.cpp", f"-D{BM=} -D{BN=} -D{BK=} -D{WAVE_M=} -D{WAVE_N=}  -D{REG_M=} -D{REG_N=} -D{REG_K=} -D{N_WAVES=}  -D{MFMA_KL=}")
 run_mfma = hip.run_mfma
 for i in range(2):
     with pyhip.cudaPerf(M*N*K*2):
         run_mfma([M_WGS, N_WGS],[64*M_WAVES*N_WAVES], A.data_ptr(), B.data_ptr(), out.data_ptr(), K, N)
 out_ref=torch.matmul(A,B_tr)
-# print(f'ref:{out_ref}')
-# print(f'out:{out}')
-# assert torch.allclose(out_ref.to(torch.float32), out, atol=0.1, rtol=0.1)
+# print(A[0:32])
+# print("-------------------------")
+# print(A[32:64])
+# print(f'ref:{out_ref[32:48]}')
+# print(f'out:{out[32:48]}')
+assert torch.allclose(out_ref.to(torch.float32), out, atol=0.1, rtol=0.1)
 
