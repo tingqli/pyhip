23b19248ebb0f5a23d180288e07eab21befb10c2
LuweiZhou <luwei.zhou@amd.com>
2025-12-27 13:28:50 +0800

    update.

--- tests/gemm/gemm.cpp
+++ tests/gemm/gemm.cpp
@@ -33,45 +33,46 @@ __global__ void __launch_bounds__(256, 1) run_mfma(__fp16* A_ptr, __fp16* B_ptr,
     int colid = lane / 32;
     int wgid_m = blockIdx.x;
     int wgid_n = blockIdx.y;
-    __fp16* A_warp = A_ptr + (wgid_m*BM + warp_id_m*WAVE_M)*K;
-    __fp16* B_warp = B_ptr + (wgid_n*BN + warp_id_n*WAVE_N)*K;
+    __fp16* A_ptr_lane = A_ptr + (wgid_m*BM + warp_id_m*WAVE_M)*K + rowid*K + colid*(REG_K*MFMA_KL);
+    __fp16* B_ptr_lane = B_ptr + (wgid_n*BN + warp_id_n*WAVE_N)*K + rowid*K + colid*(REG_K*MFMA_KL);
     float* C_warp = C_ptr + (wgid_m*BM + warp_id_m*WAVE_M)*N + (wgid_n*BN + warp_id_n*WAVE_N);
 
     //REGK is register factor based on MFMA ISA. Here 2 REGK are combined to meet minim 128 bit HBM read for coalescing tranction.
     float16x8 a[REG_M*REG_K/2];
     float16x8 b[REG_N*REG_K/2];
     float32x16 c[REG_M*REG_N] = {0};
-    auto tmp_A = A_warp + rowid*K + colid*(REG_K*MFMA_KL);
-    #pragma unroll
-    for(int m = 0; m < REG_M; m++) {
+    for(int k_idx = 0; k_idx < K; k_idx += BK)
+    {
+        auto tmp_A = A_ptr_lane;
         #pragma unroll
-        //Contineous reading the REG_K*MFMA_KL
-        for (int i = 0; i < REG_K/2; i++) {
-            a[m*REG_K/2 + i] = *(float16x8*)(tmp_A+i*8);
+        for(int m = 0; m < REG_M; m++) {
+            #pragma unroll
+            //Contineous reading the REG_K*MFMA_KL
+            for (int i = 0; i < REG_K/2; i++) {
+                a[m*REG_K/2 + i] = *(float16x8*)(tmp_A+i*8);
+            }
+            tmp_A += 32*K;
         }
-        tmp_A += 32*K;
-    }
-    // {
-    //     auto& aa=a[0];
-    //     printf("[%d, colid:%d]: %f, %f, %f, %f, %f, %f, %f,%f\n", rowid, colid,aa[0],aa[1],aa[2],aa[3],aa[4],aa[5],aa[6],aa[7]);
-    // }
-    // {
-    //     auto& aa=a[1];
-    //     printf("[%d, colid:%d]: %f, %f, %f, %f, %f, %f, %f,%f\n", rowid, colid,aa[0],aa[1],aa[2],aa[3],aa[4],aa[5],aa[6],aa[7]);
-    // }
+        // {
+        //     auto& aa=a[0];
+        //     printf("[%d, colid:%d]: %f, %f, %f, %f, %f, %f, %f,%f\n", rowid, colid,aa[0],aa[1],aa[2],aa[3],aa[4],aa[5],aa[6],aa[7]);
+        // }
+        // {
+        //     auto& aa=a[1];
+        //     printf("[%d, colid:%d]: %f, %f, %f, %f, %f, %f, %f,%f\n", rowid, colid,aa[0],aa[1],aa[2],aa[3],aa[4],aa[5],aa[6],aa[7]);
+        // }
 
-
-    auto tmp_B = B_warp + rowid*K + colid*(REG_K*MFMA_KL);
-    #pragma unroll
-    for(int n = 0; n < REG_N; n++) {
+        auto tmp_B = B_ptr_lane;
         #pragma unroll
-        for (int i = 0; i < REG_K/2; i++) {
-            b[n*REG_K/2 + i] = *(float16x8*)(tmp_B+i*8);
+        for(int n = 0; n < REG_N; n++) {
+            #pragma unroll
+            for (int i = 0; i < REG_K/2; i++) {
+                b[n*REG_K/2 + i] = *(float16x8*)(tmp_B+i*8);
+            }
+            tmp_B += 32*K;
         }
-        tmp_B += 32*K;
-    }
-
-    // for(int k = 0; k < K; k += BK) {
+        A_ptr_lane += BK;
+        B_ptr_lane += BK;
         #pragma unroll
         for(int m = 0; m < REG_M; m++) {
             #pragma unroll
@@ -82,10 +83,10 @@ __global__ void __launch_bounds__(256, 1) run_mfma(__fp16* A_ptr, __fp16* B_ptr,
                     c[idx] = __builtin_amdgcn_mfma_f32_32x32x8f16(a[m*REG_K/2 + i].lo, b[n*REG_K/2 + i].lo, c[idx], 0, 0, 0);
                     c[idx] = __builtin_amdgcn_mfma_f32_32x32x8f16(a[m*REG_K/2 + i].hi, b[n*REG_K/2 + i].hi, c[idx], 0, 0, 0);
                 }
-
             }
         }
-    // }
+    }
+
     #pragma unroll
     for(int m = 0; m < REG_M; m++) {
         #pragma unroll
