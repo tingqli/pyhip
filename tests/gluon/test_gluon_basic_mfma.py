db577bc385ab06caaa3baea7e7714be1954c676c
Luwei Zhou <luwei.zhou@amd.com>
2026-03-31 12:29:20 +0000

    [Gluon] [test] add MFMA basic test with preshuffle.

--- tests/gluon/test_gluon_basic_mfma.py
+++ tests/gluon/test_gluon_basic_mfma.py
@@ -21,7 +21,9 @@ def init_env():
     torch.set_default_device('cuda')
     torch.manual_seed(0)
 
-def gemm_test():
+def gemm_test(bpreshuffle = False):
+    from aiter.ops.shuffle import shuffle_weight
+
     mfaM = 16
     mfaN = 16
     mfaK = 32
@@ -33,17 +35,22 @@ def gemm_test():
     K = mfaK * tileK
     A = torch.randn([M, K], dtype=torch.bfloat16)*0.1
     B = torch.randn([N, K], dtype=torch.bfloat16)*0.1
+    if bpreshuffle:
+        weight = shuffle_weight(B)
+    else:
+        weight = B
     C = torch.zeros([M, N], dtype=torch.float32)
     C_ref = A @ B.T
 
     gemm_test_kernel[(1,)](
-                A,           # bf16 [M, K]
-                B,           # bf16 [N, K]
-                C,           # bf16 [M, N]
+                A,
+                weight,
+                C,
                 K=K,
                 N=N,
                 tileM = tileM,
                 tileN = tileN,
+                preshffuleB = bpreshuffle,
                 num_warps=1)
     assert torch.allclose(C_ref.to(dtype=torch.float32), C, rtol=0.1, atol=0.03)
 
@@ -65,6 +72,17 @@ def get_mem_ab_layout(tiles:gl.constexpr):
                                         block_bases=[],
                                         shape=[blk, 32])
     return mem_ab_layout
+
+# [ n//16 ,K//32,4k,16n,8k,]
+@gluon.constexpr_function
+def get_mem_preshffule_layout():
+    
+    mem_b_layout: gl.constexpr = gl.BlockedLayout(size_per_thread=[8, 1],
+                                            threads_per_warp=[64, 1],
+                                            warps_per_cta=[1, 1],
+                                            order=[0, 1])
+    return mem_b_layout
+
 
 @gluon.constexpr_function
 def get_mem_c_layout(tileM:gl.constexpr, tileN:gl.constexpr):
@@ -103,23 +121,31 @@ def gemm_test_kernel(
     N:gl.constexpr,
     tileM:gl.constexpr,
     tileN:gl.constexpr,
+    preshffuleB: gl.constexpr,
     num_warps: gl.constexpr = 1,
 ):
     BK : gl.constexpr = 32
 
     mem_a_layout: gl.constexpr = get_mem_ab_layout(tileM)
-    mem_b_layout: gl.constexpr = get_mem_ab_layout(tileN)
+    if preshffuleB:
+        mem_b_layout: gl.constexpr = get_mem_preshffule_layout()
+    else:
+        mem_b_layout: gl.constexpr = get_mem_ab_layout(tileN)
 
     mem_a_offset_m = gl.arange(0, 16*tileM, layout=gl.SliceLayout(1, mem_a_layout))
     mem_a_offset_k = gl.arange(0, BK, layout=gl.SliceLayout(0, mem_a_layout))
     mem_a_offset = mem_a_offset_m[:,None] * K + mem_a_offset_k[None,:]
     
-    mem_b_offset_n = gl.arange(0, 16*tileN, layout=gl.SliceLayout(1, mem_b_layout))
-    mem_b_offset_k = gl.arange(0, BK, layout=gl.SliceLayout(0, mem_b_layout))
-    mem_b_offset = mem_b_offset_n[:,None] * K + mem_b_offset_k[None,:]
-    
-    mem_c_layout: gl.constexpr = get_mem_c_layout(tileM=tileM, tileN=tileN)
+    if preshffuleB:
+        mem_b_offset_n = gl.arange(0, tileN, layout=gl.SliceLayout(0, mem_b_layout))
+        mem_b_offset_k = gl.arange(0, 64*8, layout=gl.SliceLayout(1, mem_b_layout))                
+        mem_b_offset = mem_b_offset_n[None,:] * K * 16 + mem_b_offset_k[:,None]
+    else:
+        mem_b_offset_n = gl.arange(0, 16*tileN, layout=gl.SliceLayout(1, mem_b_layout))
+        mem_b_offset_k = gl.arange(0, BK, layout=gl.SliceLayout(0, mem_b_layout))
+        mem_b_offset = mem_b_offset_n[:,None] * K + mem_b_offset_k[None,:]
 
+    mem_c_layout: gl.constexpr = get_mem_c_layout(tileM=tileM, tileN=tileN)
     c_layout: gl.constexpr = gl.amd.AMDMFMALayout(version=4,
                                             instr_shape=[16, 16, 32],
                                             transposed=True,
@@ -129,18 +155,23 @@ def gemm_test_kernel(
     b_fma_layout: gl.constexpr = gl.DotOperandLayout(1, c_layout, k_width=8)
     acc = gl.zeros((16*tileM, 16*tileN), dtype=gl.float32, layout=c_layout)
     for k_start in gl.static_range(0, K, BK):
-        # ab_offset = mem_ab_offset + k_start
         a = gl.amd.cdna3.buffer_load(p_input, mem_a_offset)
         a = a.reshape(16*tileM, 4, 8).reshape(16*tileM, 32)
         a_fma = gl.convert_layout(a, a_fma_layout, assert_trivial=True)
-        
         b = gl.amd.cdna3.buffer_load(p_weight, mem_b_offset)
-        b = b.reshape(16*tileN, 4, 8).permute(1, 2, 0).reshape(32, 16*tileN)
+        if preshffuleB:
+            #[N//16, K//32, 4k, 16n, 8k]
+            #[4k, 16N, 8k, N//16] -> [4k, 8k, N//16, 16N, ]
+            b = b.reshape(4, 16, 8, tileN).permute(0, 2, 3, 1).reshape(32, 16*tileN)
+        else:
+            b = b.reshape(16*tileN, 4, 8).permute(1, 2, 0).reshape(32, 16*tileN)
         b_fma = gl.convert_layout(b, b_fma_layout, assert_trivial=True)
-        
         acc = gl.amd.cdna4.mfma(a_fma, b_fma, acc)
         mem_a_offset += BK
-        mem_b_offset += BK
+        if preshffuleB:
+            mem_b_offset += BK*16
+        else:
+            mem_b_offset += BK
         
     out_offsets_m = (gl.arange(0, 16*tileM, layout=gl.SliceLayout(1, mem_c_layout)))
     out_offsets_n = (gl.arange(0, 16*tileN, layout=gl.SliceLayout(0, mem_c_layout)))
@@ -148,4 +179,6 @@ def gemm_test_kernel(
     gl.amd.cdna3.buffer_store(gl.convert_layout(acc.reshape(16*tileM, 16*tileN), mem_c_layout, assert_trivial=True), p_output, out_offsets)
 
 init_env()
-gemm_test()
+gemm_test(bpreshuffle=False)
+gemm_test(bpreshuffle=True)
+
