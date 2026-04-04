cd9c22933aee5cddc37f67a460f25fb41ecf08b2
root <root@smci350-zts-gtu-b20-05.zts-gtu.dcgpu>
2026-04-04 13:48:49 +0000

    update.

--- tests/gluon/test_gluon_basic_mfma.py
+++ tests/gluon/test_gluon_basic_mfma.py
@@ -21,7 +21,8 @@ def init_env():
     torch.set_default_device('cuda')
     torch.manual_seed(0)
 
-def gemm_test(bpreshuffle = False):
+def gemm_test(bpreshuffle = False, transposeB = False):
+    assert not (bpreshuffle and transposeB), f'weight can not be preshuffled and transposed'
     from aiter.ops.shuffle import shuffle_weight
 
     mfaM = 16
@@ -29,7 +30,7 @@ def gemm_test(bpreshuffle = False):
     mfaK = 32
     tileM = 2
     tileN = 2
-    tileK = 8
+    tileK = 10
     M = mfaM * tileM
     N = mfaN * tileN
     K = mfaK * tileK
@@ -38,7 +39,10 @@ def gemm_test(bpreshuffle = False):
     if bpreshuffle:
         weight = shuffle_weight(B)
     else:
-        weight = B
+        # for the B weight(N=16, K=32) torch tensor. B.t() and B would have the same buffer but with diffrent shape and stride description
+        # B.shape = [16, 32] B.stride() = [32, 1], B.t().shape = [32, 16], B.t().stride() = [1, 32]. B and B.t() share same buffer layout, but shape
+        # and stride are both permuted. Since the linear layout is also descriping the shape and stride, which is similiar with torch tensor shape and stride()
+        weight = B.t() if transposeB else B
     C = torch.zeros([M, N], dtype=torch.float32)
     C_ref = A @ B.T
 
@@ -53,7 +57,14 @@ def gemm_test(bpreshuffle = False):
                 preshffuleB = bpreshuffle,
                 num_warps=1)
     assert torch.allclose(C_ref.to(dtype=torch.float32), C, rtol=0.1, atol=0.03)
+    print(f"pass {M=} {N=} {K=} {bpreshuffle}")
 
+
+#linear layout， 根据reg_bases->lane_bases->warp_base顺序tile数据，在每一层里，描述
+#哪个维度先被tile进GPU寄存器。本测试中B tensor是[N, K]的layout，无论传输kernel的是b 还是
+# b.t(),都采用同样的linear layout, b和b.t()实际是物理内存的排布是不变的。
+# 这里的表述是对于64个lane,先用16个lane 排布dimention[0],然后是dimmention[1].由于 b和b.t()
+# 有相同的物理内存排布（[N,K]），可以使用相同的linear layout.
 @gluon.constexpr_function
 def get_mem_ab_layout(tiles:gl.constexpr):
     reg_bases = [[0, 1], [0, 2], [0, 4]]
@@ -76,11 +87,17 @@ def get_mem_ab_layout(tiles:gl.constexpr):
 # [ n//16 ,K//32,4k,16n,8k,]
 @gluon.constexpr_function
 def get_mem_preshffule_layout():
-    
-    mem_b_layout: gl.constexpr = gl.BlockedLayout(size_per_thread=[8, 1],
-                                            threads_per_warp=[64, 1],
+    # 如何理解order,如果把order简单的理解成为行优先或者列优先,将对后面的reshape, permute操作很难理解。
+    # blocked layout和linear layout的本质上都是在描述, reg, lane , wave与tensor的映射关系。 相当于pyhip里
+    # 每个lane需要找到这条lane对应的m, n.
+    # 在tensor的每个维度上都将按照reg-> lane -> wave的顺序d读取并堆积(tile)数据到GPU VGPRs，这里的order实际上在描述维度
+    # 堆积的顺序（当然通常最先堆积的维度时物理排布连续的）。但是如果理解成tile的顺序，就把实际tensor的物理内存排布与
+    # 映射关系解耦，比如说，这个表示每个thread先排8个dimension 1的数据排进8个寄存器（通常时连续寄存器）。至于这8个数据是否在外存连续，
+    # 这里可以暂时不考虑。
+    mem_b_layout: gl.constexpr = gl.BlockedLayout(size_per_thread=[1, 8],
+                                            threads_per_warp=[1, 64],
                                             warps_per_cta=[1, 1],
-                                            order=[0, 1])
+                                            order=[1, 0])
     return mem_b_layout
 
 
@@ -132,14 +149,15 @@ def gemm_test_kernel(
     else:
         mem_b_layout: gl.constexpr = get_mem_ab_layout(tileN)
 
+    #获得每个lane mapping的 m, k，并根据mk以及实际的layout计算offset, 从HBM中读取。
     mem_a_offset_m = gl.arange(0, 16*tileM, layout=gl.SliceLayout(1, mem_a_layout))
     mem_a_offset_k = gl.arange(0, BK, layout=gl.SliceLayout(0, mem_a_layout))
     mem_a_offset = mem_a_offset_m[:,None] * K + mem_a_offset_k[None,:]
     
     if preshffuleB:
-        mem_b_offset_n = gl.arange(0, tileN, layout=gl.SliceLayout(0, mem_b_layout))
-        mem_b_offset_k = gl.arange(0, 64*8, layout=gl.SliceLayout(1, mem_b_layout))                
-        mem_b_offset = mem_b_offset_n[None,:] * K * 16 + mem_b_offset_k[:,None]
+        mem_b_offset_n = gl.arange(0, tileN, layout=gl.SliceLayout(1, mem_b_layout)) #[tileN]
+        mem_b_offset_k = gl.arange(0, 64*8, layout=gl.SliceLayout(0, mem_b_layout))  # [64*8]              
+        mem_b_offset = mem_b_offset_n[:, None] * K * 16 + mem_b_offset_k[None,:] # [tileN,64*8]
     else:
         mem_b_offset_n = gl.arange(0, 16*tileN, layout=gl.SliceLayout(1, mem_b_layout))
         mem_b_offset_k = gl.arange(0, BK, layout=gl.SliceLayout(0, mem_b_layout))
@@ -154,17 +172,36 @@ def gemm_test_kernel(
     a_fma_layout: gl.constexpr = gl.DotOperandLayout(0, c_layout, k_width=8)
     b_fma_layout: gl.constexpr = gl.DotOperandLayout(1, c_layout, k_width=8)
     acc = gl.zeros((16*tileM, 16*tileN), dtype=gl.float32, layout=c_layout)
+    
+    # gl.static_print(">>>>>> fma_A ", gl.to_linear_layout(a_fma_layout, [16, 32]))
+    # gl.static_print(">>>>>> fma_B ", gl.to_linear_layout(b_fma_layout, [32, 16]))
+    # gl.static_print(">>>>>> fma_C ", gl.to_linear_layout(c_layout, [16, 16]))
     for k_start in gl.static_range(0, K, BK):
+        # gl.static_print(">>>>>>?>>>>>>>>>>>")
+
         a = gl.amd.cdna3.buffer_load(p_input, mem_a_offset)
         a = a.reshape(16*tileM, 4, 8).reshape(16*tileM, 32)
         a_fma = gl.convert_layout(a, a_fma_layout, assert_trivial=True)
         b = gl.amd.cdna3.buffer_load(p_weight, mem_b_offset)
         if preshffuleB:
-            #[N//16, K//32, 4k, 16n, 8k]
-            #[4k, 16N, 8k, N//16] -> [4k, 8k, N//16, 16N, ]
-            b = b.reshape(4, 16, 8, tileN).permute(0, 2, 3, 1).reshape(32, 16*tileN)
+            if k_start == 0:
+                gl.static_print(">>>>>> layout0 ", b.type)
+                #  <['2', '512'], bf16, BlockedLayout(size_per_thread=[1, 8], threads_per_warp=[1, 64], warps_per_cta=[1, 1], order=[1, 0], cga_layout=[])>
+                #  在第一个维度按lane顺序，每个tile 8. preshuffle的layout[tileN, 4k, 16n, 8k] squeeze成[tileN, 64x8]
+                gl.static_print(">>>>>> layout0,+reshape ", b.reshape(tileN, 4, 16, 8).type)
+                #  <['2', '4', '16', '8'], bf16, BlockedLayout(size_per_thread=[1, 1, 1, 8], threads_per_warp=[1, 4, 16, 1], warps_per_cta=[1, 1, 1, 1], order=[3, 2, 1, 0], cga_layout=[]
+                # ['2', '4', '16', '8'] is [tileN, 4k, 16n, 8k],根据order的顺序是 先tile 8k, 然后16n, 然后4k, 然后tileN
+                gl.static_print(">>>>>> layout0,+permute ", b.reshape(tileN, 4, 16, 8).permute(1, 3, 0, 2).type)
+                #  <['4', '8', '2', '16'], bf16, BlockedLayout(size_per_thread=[1, 8, 1, 1], threads_per_warp=[4, 1, 1, 16], warps_per_cta=[1, 1, 1, 1], order=[1, 3, 0, 2], cga_layout=[])>
+                # ['4', '8', '2', '16'] is [4k, 8k, tileN 16n],根据order的顺序[1, 3, 0, 2], 是 先tile 8k, 然后16n, 然后4k, 然后tileN
+                gl.static_print(">>>>>> layout0 ", b.reshape(tileN, 4, 16, 8).permute(1, 3, 0, 2).reshape(32, 16*tileN).type)
+                #  <['32', '32'], bf16, DistributedLinearLayout(reg_bases=[[1, 0], [2, 0], [4, 0], [0, 16]], lane_bases=[[0, 1], [0, 2], [0, 4], [0, 8], [8, 0], [16, 0]], warp_bases=[], block_bases=[], shape=[32, 32])>
+            
+            #[tileN, 64lane*8k] -> [tileN, 4k, 16n, 8k] -> [4k, 8k, tileN, 16n,] -> [32k, tileN*16N]
+            # The process is like the reverse process of preshuffle.
+            b = b.reshape(tileN, 4, 16, 8).permute(1, 3, 0, 2).reshape(32, 16*tileN)
         else:
-            b = b.reshape(16*tileN, 4, 8).permute(1, 2, 0).reshape(32, 16*tileN)
+            b = b.permute(1, 0)
         b_fma = gl.convert_layout(b, b_fma_layout, assert_trivial=True)
         acc = gl.amd.cdna4.mfma(a_fma, b_fma, acc)
         mem_a_offset += BK
@@ -179,6 +216,7 @@ def gemm_test_kernel(
     gl.amd.cdna3.buffer_store(gl.convert_layout(acc.reshape(16*tileM, 16*tileN), mem_c_layout, assert_trivial=True), p_output, out_offsets)
 
 init_env()
-gemm_test(bpreshuffle=False)
+gemm_test(bpreshuffle=False, transposeB = True)
+gemm_test(bpreshuffle=False, transposeB = False)
 gemm_test(bpreshuffle=True)
 
