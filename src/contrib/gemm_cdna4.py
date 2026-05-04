de72467707bd8aef49c7bb224c5f134b6acfc548
root <root@smci350-rck-g03-b19-03.rck.dcgpu>
2026-05-04 08:29:06 +0000

    test.

--- src/contrib/gemm_cdna4.py
+++ src/contrib/gemm_cdna4.py
@@ -9,7 +9,8 @@ __all__ = [
 @pyhip.jit()
 def gemm_kernel(J, wg_M, wg_N, N, K, use_pre_shuffle, pA:"void*", pB:"void*", pC:"void*", M:"int"):
     # 128 bytes
-    wg_K = J.div(128, J.sizeof_bf16)
+    # wg_K = J.div(128, J.sizeof_bf16)
+    wg_K = 64
 
     A_dtype = "bf16"
     B_dtype = "bf16"
@@ -31,21 +32,21 @@ def gemm_kernel(J, wg_M, wg_N, N, K, use_pre_shuffle, pA:"void*", pB:"void*", pC
     assert N % wg_N == 0
     assert K % wg_K == 0
     num_warps = 4
-    nbN = J.div(wg_N, 16)
-    nbM = J.div(wg_M, 16)
-    nbK = 2 # 2 MFMA 16x16 
+    nbN = J.div(wg_N, 16)# the N tile number per WG, each N tile 16n
+    nbM = J.div(wg_M, 16)# the M tile number per WG, each M tile 16m
+    nbK = 2 # 2 MFMA 16x16 # the k tile number per WG, each K tile 32k
     buff_a = J.Buffer(pA, (M1 - M0) * stride_k)
     buff_b = J.Buffer(pB, wg_N * stride_k)
     buff_c = J.Buffer(pC, (M1 - M0) * stride_c)
     ldsA = [J.alloc_lds(nbM * nbK * 1024), J.alloc_lds(nbM * nbK * 1024)]
     ldsB = [J.alloc_lds(nbN * nbK * 1024), J.alloc_lds(nbN * nbK * 1024)]
 
-    nrM = J.div(nbM, 2)
-    nrN = J.div(nbN, 2)
-    nrK = nbK
+    nrM = J.div(nbM, 2) # the N tile number per warp, each N tile 16n
+    nrN = J.div(nbN, 2) # the N tile number per warp, each M tile 16n
+    nrK = nbK           # the k tile number per warp, each K tile 32k
 
-    warp_m = J.gpr((J.warp_id[0] // 2)*nrM)
-    warp_n = J.gpr((J.warp_id[0] % 2)*nrN)
+    warp_m = J.gpr((J.warp_id[0] // 2)*nrM) # M start tile idx of the warp inside the one WG
+    warp_n = J.gpr((J.warp_id[0] % 2)*nrN)  # N start tile idx of the warp inside the one WG
 
     num_warps = 4
     vm_load_a, vm_load_cnt_a, vm_offset_inc_a, ds_read_a = get_mfma_loader(J, use_pre_shuffle, num_warps, wg_M, 128, stride_k, warp_m*16)
@@ -53,7 +54,7 @@ def gemm_kernel(J, wg_M, wg_N, N, K, use_pre_shuffle, pA:"void*", pB:"void*", pC
 
     print(f"============={nbM=}, {nbN=}, {nbK=} {nrM=} {nrN=} {nrK=}")
 
-    mfma_A = J.gpr(2, nrM, 4, "vbf16x2")
+    mfma_A = J.gpr(2, nrM, 4, "vbf16x2") # 2 is 64k is divided into 2 part
     mfma_B = J.gpr(2, nrN, 4, "vbf16x2")
     mfma_C = J.gpr(nrM, nrN, 4, "af32")
 
@@ -62,115 +63,141 @@ def gemm_kernel(J, wg_M, wg_N, N, K, use_pre_shuffle, pA:"void*", pB:"void*", pC
             for n in range(nrN):
                 J.v_mfma_f32_16x16x32_bf16(mfma_C[m,n], mfma_B[reg_id, n], mfma_A[reg_id, m], mfma_C[m,n])
                 yield 16
+    if 0:
+        koffset_a = J.gpr("su32", 0)
+        koffset_b = J.gpr("su32", 0)
+        mfma_C[...] = 0
+        def loop_body():
+            J.s_barrier()
+            J.emit(vm_load_a(ldsA[0], buff_a, koffset_a))
+            J.emit(vm_load_b(ldsB[0], buff_b, koffset_b))
+            koffset_a[0] += vm_offset_inc_a
+            koffset_b[0] += vm_offset_inc_b
+            J.s_waitcnt(mod=f"vmcnt({0})")
+            J.s_barrier()
 
-    koffset_a = J.gpr("su32", 0)
-    koffset_b = J.gpr("su32", 0)
-    J.emit(vm_load_a(ldsA[0], buff_a, koffset_a))
-    J.emit(vm_load_b(ldsB[0], buff_b, koffset_b))
-    koffset_a[0] += vm_offset_inc_a
-    koffset_b[0] += vm_offset_inc_b
+            for m in range(nrM): ds_read_a(ldsA[0], mfma_A[0, m], m, 0)
+            for n in range(nrN): ds_read_b(ldsB[0], mfma_B[0, n], n, 0)            
+            for m in range(nrM): ds_read_a(ldsA[0], mfma_A[1, m], m, 1)
+            for n in range(nrN): ds_read_b(ldsB[0], mfma_B[1, n], n, 1)
+            J.s_waitcnt(mod=f"lgkmcnt(0)")
+            mfma_ab0=mfma(0)
+            mfma_ab1=mfma(1)
+            J.emit(mfma_ab0)
+            J.emit(mfma_ab1)
+            
+        for koff in range(K//wg_K):
+            loop_body()
 
-    J.emit(vm_load_a(ldsA[1], buff_a, koffset_a))
-    J.emit(vm_load_b(ldsB[1], buff_b, koffset_b))
-    koffset_a[0] += vm_offset_inc_a
-    koffset_b[0] += vm_offset_inc_b
-
-    mfma_C[...] = 0
-
-    '''
-    ab0: mfma ab0 | ds_read ab1; wait_lgkmcnt(0), barrier; vm-load a01
-    ab1: mfma ab1 | vm-load b01; wait_vmcnt, barrier, ds_read ab2
-
-    ab2: mfma ab2 | ds_read ab3; wait_lgkmcnt(0), barrier; vm-load  a23
-    ab3: mfma ab3 | vm-load b23;  wait_vmcnt, barrier, ds_read ab0
-    '''
-    J.s_waitcnt(mod=f"vmcnt({vm_load_cnt_b + vm_load_cnt_a})")
-    J.s_barrier()
-
-    # ds_read ab0
-    for m in range(nrM): ds_read_a(ldsA[0], mfma_A[0, m], m, 0)
-    for n in range(nrN): ds_read_b(ldsB[0], mfma_B[0, n], n, 0)
-    J.s_waitcnt(mod=f"lgkmcnt(0)")
-
-    def loop_body(lds_id):
-        # mfma ab0
-        mfma_ab0 = mfma(0)
-
-        # ds_read ab1
-        for m in range(nrM):
-            J.emit(mfma_ab0, 16)
-            ds_read_a(ldsA[lds_id], mfma_A[1, m], m, 1)
-        for n in range(nrN):
-            J.emit(mfma_ab0, 16)
-            ds_read_b(ldsB[lds_id], mfma_B[1, n], n, 1)
-
-        J.emit(mfma_ab0, 64)
-
-        J.s_waitcnt(mod=f"lgkmcnt(0)")
-        J.emit(mfma_ab0, 16)
-        J.s_barrier()
-
-        mfma_ab1 = mfma(1)
-
-        # vm-load a01
-        vm_load = vm_load_a(ldsA[lds_id], buff_a, koffset_a)
-        J.emit([mfma_ab0, mfma_ab1], 16)
-        J.emit(vm_load, 1) # first emit produce preparing instructions
-        J.emit([mfma_ab0, mfma_ab1], 16)
-        for _ in range(vm_load_cnt_a):
-            J.emit(vm_load, 1)
-            J.emit([mfma_ab0, mfma_ab1], 96)
-        J.emit(vm_load)
-
-        # vm-load b01
-        vm_load = vm_load_b(ldsB[lds_id], buff_b, koffset_b)
-        J.emit([mfma_ab0, mfma_ab1], 16)
-        J.emit(vm_load, 1) # first emit produce preparing instructions
-        J.emit([mfma_ab0, mfma_ab1], 16)
-        for _ in range(vm_load_cnt_b - 4):
-            J.emit(vm_load, 1)
-            J.emit([mfma_ab0, mfma_ab1], 96)
-
-        J.emit(mfma_ab0) # emit all MFMA using AB register0 (since ds_read will override it)
-
-        # wait vm-load a23/b23 to finish
-        J.s_waitcnt(mod=f"vmcnt({vm_load_cnt_b + vm_load_cnt_a - 4})")
-        J.s_barrier()
-
-        J.emit(vm_load, 1)
-        # ds_read ab2
-        for m in range(nrM):
-            J.emit(mfma_ab1, 16)
-            ds_read_a(ldsA[(lds_id + 1)&1], mfma_A[0, m], m, 0)
-
-        J.emit(vm_load, 1)
-        for n in range(nrN):
-            J.emit(mfma_ab1, 16)
-            ds_read_b(ldsB[(lds_id + 1)&1], mfma_B[0, n], n, 0)
-
-        J.emit(vm_load, 1)
-        J.emit(mfma_ab1, 96)
-        J.emit(vm_load, 1)
-        J.emit(mfma_ab1)
-
+    else:
+        koffset_a = J.gpr("su32", 0)
+        koffset_b = J.gpr("su32", 0)
+        J.emit(vm_load_a(ldsA[0], buff_a, koffset_a))
+        J.emit(vm_load_b(ldsB[0], buff_b, koffset_b))
         koffset_a[0] += vm_offset_inc_a
         koffset_b[0] += vm_offset_inc_b
 
+        J.emit(vm_load_a(ldsA[1], buff_a, koffset_a))
+        J.emit(vm_load_b(ldsB[1], buff_b, koffset_b))
+        koffset_a[0] += vm_offset_inc_a
+        koffset_b[0] += vm_offset_inc_b
+
+        mfma_C[...] = 0
+
+        '''
+        ab0: mfma ab0 | ds_read ab1; wait_lgkmcnt(0), barrier; vm-load a01
+        ab1: mfma ab1 | vm-load b01; wait_vmcnt, barrier, ds_read ab2
+
+        ab2: mfma ab2 | ds_read ab3; wait_lgkmcnt(0), barrier; vm-load  a23
+        ab3: mfma ab3 | vm-load b23;  wait_vmcnt, barrier, ds_read ab0
+        '''
+        J.s_waitcnt(mod=f"vmcnt({vm_load_cnt_b + vm_load_cnt_a})")
+        J.s_barrier()
+
+        # ds_read ab0
+        for m in range(nrM): ds_read_a(ldsA[0], mfma_A[0, m], m, 0)
+        for n in range(nrN): ds_read_b(ldsB[0], mfma_B[0, n], n, 0)
         J.s_waitcnt(mod=f"lgkmcnt(0)")
 
-    if 0:
-        for koff in range(J.div(K, wg_K)):
-            loop_body(koff & 1)
-    else:
-        koff = J.gpr("su32", 0)
-        loop_cnt = K // (2*wg_K)
-        with J.While(koff[0] < loop_cnt):
-            loop_body(0)
-            loop_body(1)
-            koff[0] += 1
+        def loop_body(lds_id):
+            # mfma ab0
+            mfma_ab0 = mfma(0)
 
-        if K % (2*wg_K):
-            loop_body(0)
+            # ds_read ab1
+            for m in range(nrM):
+                J.emit(mfma_ab0, 16)
+                ds_read_a(ldsA[lds_id], mfma_A[1, m], m, 1)
+            for n in range(nrN):
+                J.emit(mfma_ab0, 16)
+                ds_read_b(ldsB[lds_id], mfma_B[1, n], n, 1)
+
+            J.emit(mfma_ab0, 64)
+
+            J.s_waitcnt(mod=f"lgkmcnt(0)")
+            J.emit(mfma_ab0, 16)
+            J.s_barrier()
+
+            mfma_ab1 = mfma(1)
+
+            # vm-load a01
+            vm_load = vm_load_a(ldsA[lds_id], buff_a, koffset_a)
+            J.emit([mfma_ab0, mfma_ab1], 16)
+            J.emit(vm_load, 1) # first emit produce preparing instructions
+            J.emit([mfma_ab0, mfma_ab1], 16)
+            for _ in range(vm_load_cnt_a):
+                J.emit(vm_load, 1)
+                J.emit([mfma_ab0, mfma_ab1], 96)
+            J.emit(vm_load)
+
+            # vm-load b01
+            vm_load = vm_load_b(ldsB[lds_id], buff_b, koffset_b)
+            J.emit([mfma_ab0, mfma_ab1], 16)
+            J.emit(vm_load, 1) # first emit produce preparing instructions
+            J.emit([mfma_ab0, mfma_ab1], 16)
+            for _ in range(vm_load_cnt_b - 4):
+                J.emit(vm_load, 1)
+                J.emit([mfma_ab0, mfma_ab1], 96)
+
+            J.emit(mfma_ab0) # emit all MFMA using AB register0 (since ds_read will override it)
+
+            # wait vm-load a23/b23 to finish
+            J.s_waitcnt(mod=f"vmcnt({vm_load_cnt_b + vm_load_cnt_a - 4})")
+            J.s_barrier()
+
+            J.emit(vm_load, 1)
+            # ds_read ab2
+            for m in range(nrM):
+                J.emit(mfma_ab1, 16)
+                ds_read_a(ldsA[(lds_id + 1)&1], mfma_A[0, m], m, 0)
+
+            J.emit(vm_load, 1)
+            for n in range(nrN):
+                J.emit(mfma_ab1, 16)
+                ds_read_b(ldsB[(lds_id + 1)&1], mfma_B[0, n], n, 0)
+
+            J.emit(vm_load, 1)
+            J.emit(mfma_ab1, 96)
+            J.emit(vm_load, 1)
+            J.emit(mfma_ab1)
+
+            koffset_a[0] += vm_offset_inc_a
+            koffset_b[0] += vm_offset_inc_b
+
+            J.s_waitcnt(mod=f"lgkmcnt(0)")
+
+        if 1:
+            for koff in range(J.div(K, wg_K)):
+                loop_body(koff & 1)
+        else:
+            koff = J.gpr("su32", 0)
+            loop_cnt = K // (2*wg_K)
+            with J.While(koff[0] < loop_cnt):
+                loop_body(0)
+                loop_body(1)
+                koff[0] += 1
+
+            if K % (2*wg_K):
+                loop_body(0)
 
     for lds in ldsA: J.free_lds(lds) 
     for lds in ldsB: J.free_lds(lds)
--- tests/contrib/gemm/test_cdna4.py
+++ tests/contrib/gemm/test_cdna4.py
@@ -100,6 +100,6 @@ def compare_perf(M, N, K, use_pre_shuffle = 0):
     print(f"{acc=}")
 
 if __name__ == "__main__":
-    test_accuracy(2400, 256*4, 256*6)
+    # test_accuracy(2400, 256*4, 256*6)
     test_accuracy(256, 256, 256)
     compare_perf(M = 256*94, N = 256*16, K=8192)
